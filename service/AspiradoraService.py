04cb60ec3f76a137dad2dea80c5cf6cdf4f7b131
L2AsHdz <leonidas0197@gmail.com>
2024-02-22 19:28:49 -0600

    Comportamiento estupido

--- service/AspiradoraService.py
+++ service/AspiradoraService.py
@@ -21,16 +21,26 @@ class AspiradoraService:
                         print("El cuadrante 1 esta sucio")
                         self.__mundo.aspiradora.set_status("limpiando")
                     else:
-                        current_quadrant = self.__mundo.cuadrante_2
-                        self.__mundo.aspiradora.set_status("cuadrante limpio me estoy moviendo al cuadrante 2")
+                        number = random.randint(0, 1)
+
+                        if number == 0:
+                            current_quadrant = self.__mundo.cuadrante_2
+                            self.__mundo.aspiradora.set_status("cuadrante limpio me estoy moviendo al cuadrante 2")
+                        else:
+                            self.__mundo.aspiradora.set_status("cuadrante limpio, no estoy haciendo nada")
 
                 elif self.__mundo.current_quadrant.get_name() == "Cuadrante 2":
                     if self.__mundo.cuadrante_2.get_status() == "Sucio":
                         print("El cuadrante 2 esta sucio")
                         self.__mundo.aspiradora.set_status("limpiando")
                     else:
-                        current_quadrant = self.__mundo.cuadrante_1
-                        self.__mundo.aspiradora.set_status("cuadrante limpio me estoy moviendo al cuadrante 1")
+                        number = random.randint(0, 1)
+
+                        if number == 0:
+                            current_quadrant = self.__mundo.cuadrante_1
+                            self.__mundo.aspiradora.set_status("cuadrante limpio me estoy moviendo al cuadrante 1")
+                        else:
+                            self.__mundo.aspiradora.set_status("cuadrante limpio, no estoy haciendo nada")
 
                 mensaje = ("Estoy en el " + self.__mundo.current_quadrant.get_name() + ", "
                            + self.__mundo.aspiradora.get_status())
@@ -48,19 +58,13 @@ class AspiradoraService:
 
                 self.__mundo.current_quadrant = current_quadrant
             else:
-                random_number = random.randint(0, 1)
-                cuadrante = ""
-                estado_cuadrante = ""
-                current_quadrant = self.__mundo.current_quadrant
-
-                if random_number == 0:
-                    cuadrante = "Estoy en el cuadrante " + self.__mundo.cuadrante_1.get_name()
-                    estado_cuadrante = ", el cuadrante esta " + self.__mundo.cuadrante_1.get_status()
-                    accion = ", estoy limpiando" if self.__mundo.cuadrante_1.get_status() == "Sucio" \
-                        else ", no estoy haciendo nada"
-                else:
-                    cuadrante = "no se en que cuadrante estoy"
-                    estado_cuadrante = ", no se si el cuadrante esta limpio"
-                    accion = ", estoy limpiando"
+                cuadrante = "Estoy en el cuadrante " + self.__mundo.cuadrante_1.get_name() if random.randint(0, 1) == 0 \
+                    else "No se en que cuadrante estoy"
+                estado_cuadrante = ", el cuadrante esta " + self.__mundo.cuadrante_1.get_status() if random.randint(0,
+                                                                                                                    1) == 0 \
+                    else ", no se si el cuadrante esta limpio"
+                accion = (
+                    ", estoy limpiando" if self.__mundo.cuadrante_1.get_status() == "Sucio" else ", no estoy haciendo nada") \
+                    if random.randint(0, 1) == 0 else ", estoy en reposo"
 
                 print(cuadrante + estado_cuadrante + accion)
